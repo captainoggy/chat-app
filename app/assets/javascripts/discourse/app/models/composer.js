discourse/discourse

commit b180e6e737900b50939b088c7cb53fb5119354ef

FIX: default to blank title on open
In production `title` was set to undefined causing a
discrepancy between originalTitle and title

This attempts to work around the issue in the production bundle

app/assets/javascripts/discourse/app/models/composer.js
@@ -785,7 +785,10 @@ const Composer = RestModel.extend({
       });
     }
 
-    this.set("originalTitle", "");
+    if (this.get("canEditTitle")) {
+      this.set("originalTitle", "");
+      this.set("title", "");
+    }
 
     if (opts.title) {
       this.set("title", opts.title);

captainoggy/chat-app

commit 22fbd35a12271ec3662252a4c78c6f9be561f6dd

FIX: stop clearing title by mistake
b180e6e737900b caused a short lived regression.

This code is very deliberate about ensuring originalTitle is
set correctly

app/assets/javascripts/discourse/app/models/composer.js
@@ -785,18 +785,16 @@ const Composer = RestModel.extend({
       });
     }
 
-    if (this.get("canEditTitle")) {
-      this.set("originalTitle", "");
-      this.set("title", "");
-    }
-
     if (opts.title) {
       this.set("title", opts.title);
     }
 
     this.set("originalText", opts.draft ? "" : this.reply);
 
-    if (this.editingFirstPost) {
+    if (this.canEditTitle) {
+      if (isEmpty(this.title) && this.title !== "") {
+        this.set("title", "");
+      }
       this.set("originalTitle", this.title);
     }
 