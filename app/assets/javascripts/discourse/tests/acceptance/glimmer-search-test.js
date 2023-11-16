discourse/discourse

commit a7ff465ca6199bfa087b87eb17363beb8bc4c0c9

FIX: Redirect to highlighted search result on 'Enter` (#24393)
Raised in https://meta.discourse.org/t/keyboard-navigation-messes-up-the-search-menu/285405

We were incorrectly accessing the highlighted search result target's href which caused issues when navigating the topic list (eg /latest) with **j / k** and then immediately after accessing the search menu and navigating to and selecting a search result with the keyboard.  

### Current Behavior
Hitting enter on a search result redirects to the href of the topic in the topic list that was previously highlighted.

### Expected Behavior
Hitting enter on a search result redirects to the href of the highlighted search result.

app/assets/javascripts/discourse/app/components/search-menu/results/types.js
@@ -23,24 +23,33 @@ export default class Types extends Component {
 
   @action
   onClick(event) {
+    this.routeToSearchResult(event);
+  }
+
+  @action
+  onKeydown(event) {
+    if (event.key === "Escape") {
+      this.args.closeSearchMenu();
+      event.preventDefault();
+      return false;
+    } else if (event.key === "Enter") {
+      this.routeToSearchResult(event);
+      return false;
+    }
+
+    this.search.handleResultInsertion(event);
+    this.search.handleArrowUpOrDown(event);
+  }
+
+  @action
+  routeToSearchResult(event) {
     if (wantsNewWindow(event)) {
       return;
     }
 
     event.preventDefault();
-    DiscourseURL.routeTo(event.currentTarget.href);
+    event.stopPropagation();
+    DiscourseURL.routeTo(event.target.href);
     this.args.closeSearchMenu();
-  }
-
-  @action
-  onKeydown(e) {
-    if (e.key === "Escape") {
-      this.args.closeSearchMenu();
-      e.preventDefault();
-      return false;
-    }
-
-    this.search.handleResultInsertion(e);
-    this.search.handleArrowUpOrDown(e);
   }
 }

app/assets/javascripts/discourse/tests/acceptance/glimmer-search-test.js
@@ -1,4 +1,10 @@
-import { click, fillIn, triggerKeyEvent, visit } from "@ember/test-helpers";
+import {
+  click,
+  currentURL,
+  fillIn,
+  triggerKeyEvent,
+  visit,
+} from "@ember/test-helpers";
 import { test } from "qunit";
 import { DEFAULT_TYPE_FILTER } from "discourse/components/search-menu";
 import searchFixtures from "discourse/tests/fixtures/search-fixtures";
@@ -481,6 +487,10 @@ acceptance("Search - Glimmer - Authenticated", function (needs) {
         ],
       })
     );
+
+    server.get("/t/2179.json", () => {
+      return helper.response({});
+    });
   });
 
   test("full page search - the right filters are shown", async function (assert) {
@@ -623,6 +633,30 @@ acceptance("Search - Glimmer - Authenticated", function (needs) {
         searchFixtures["search/query"].topics[0].slug
       ),
       "adds link from search to composer"
+    );
+  });
+
+  // see https://meta.discourse.org/t/keyboard-navigation-messes-up-the-search-menu/285405
+  test("search menu keyboard navigation - on 'Enter' keydown navigate to selected search item url", async function (assert) {
+    await visit("/");
+    await triggerKeyEvent(document, "keypress", "J");
+    await click("#search-button");
+    await fillIn("#search-term", "Development");
+    await triggerKeyEvent(document.activeElement, "keyup", "Enter");
+    await triggerKeyEvent(document.activeElement, "keyup", "ArrowDown");
+
+    const firstTopicResultUrl = "/t/development-mode-super-slow/2179";
+    assert.strictEqual(
+      document.activeElement.getAttribute("href"),
+      firstTopicResultUrl,
+      "first search result is highlighted"
+    );
+
+    await triggerKeyEvent(document.activeElement, "keydown", "Enter");
+    assert.strictEqual(
+      currentURL(),
+      firstTopicResultUrl,
+      "redirects to selected search result url"
     );
   });
 

captainoggy/chat-app

commit 124c8eecd888e1fc92624e10d08a3c7d5472350d

FIX: Navigate to search result url on click (#24414)
Broken in https://github.com/discourse/discourse/pull/24393

app/assets/javascripts/discourse/app/components/search-menu/results/types.js
@@ -23,6 +23,9 @@ export default class Types extends Component {
 
   @action
   onClick(event) {
+    if (wantsNewWindow(event)) {
+      return;
+    }
     this.routeToSearchResult(event);
   }
 
@@ -33,6 +36,8 @@ export default class Types extends Component {
       event.preventDefault();
       return false;
     } else if (event.key === "Enter") {
+      event.preventDefault();
+      event.stopPropagation();
       this.routeToSearchResult(event);
       return false;
     }
@@ -43,12 +48,6 @@ export default class Types extends Component {
 
   @action
   routeToSearchResult(event) {
-    if (wantsNewWindow(event)) {
-      return;
-    }
-
-    event.preventDefault();
-    event.stopPropagation();
     DiscourseURL.routeTo(event.target.href);
     this.args.closeSearchMenu();
   }

app/assets/javascripts/discourse/tests/acceptance/glimmer-search-test.js
@@ -534,6 +534,28 @@ acceptance("Search - Glimmer - Authenticated", function (needs) {
     );
   });
 
+  test("topic results - topic search scope - clicking a search result navigates to topic url", async function (assert) {
+    await visit("/");
+    await click("#search-button");
+    await fillIn("#search-term", "Development");
+    await triggerKeyEvent(document.activeElement, "keyup", "Enter");
+
+    const firstSearchResult =
+      ".search-menu .results li:nth-of-type(1) a.search-link";
+    const firstTopicResultUrl = "/t/development-mode-super-slow/2179";
+    assert.strictEqual(
+      query(firstSearchResult).getAttribute("href"),
+      firstTopicResultUrl
+    );
+
+    await click(firstSearchResult);
+    assert.strictEqual(
+      currentURL(),
+      firstTopicResultUrl,
+      "redirects to clicked search result url"
+    );
+  });
+
   test("search menu keyboard navigation", async function (assert) {
     const container = ".search-menu .results";
     await visit("/");